9c6cec5372708720b03fc11bbce201468a8398a1
alexis-leclerc-cegep <1942518@cegeprdl.ca>
2023-02-05 15:29:21 -0500

    ajout plusieurs tables

--- database/migrations/create_motscles_table.php
+++ database/migrations/create_motscles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateMotsClesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('Articles', function (Blueprint $table) {
+        Schema::create('MotsCles', function (Blueprint $table) {
             $table->id();
-            $table->string('titre');
-            $table->text('contenu');
-            $table->bit('approuve');
-            $table->foreign('auteur_id')->references('id')->on('Auteurs');
+            $table->string('nom');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('Auteurs');
+        Schema::dropIfExists('MotsCles');
     }
 }
--- database/migrations/create_motsclesarticles_table.php
+++ database/migrations/create_motsclesarticles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateMotsClesArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('MotsClesArticles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreign('motcle_id')->references('id')->on('MotsCles');
+            $table->foreign('article_id')->references('id')->on('Articles');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('MotsClesArticles');
     }
 }
