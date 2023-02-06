b56a85aacef52aaf6d6d9bc51f90c584ef27d5a3
Samuel Gassama <samuel@Samuels-MacBook-Air.local>
2023-02-06 16:04:37 -0500

    Intégration du template et commencement des layouts

---
+++ resources/views/aPropos.blade.php
@@ -0,0 +1,16 @@
+@extends("layouts.master")
+
+@section("title")
+About us | MyWebsite
+@endsection
+
+@section("content")
+
+  <section class="section-100">
+    <div class="container">
+      <h1>About Us</h1>
+      <p>Description of About us page here.</p>
+    </div>
+  </section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/aPropos', function () {
+    return view('aPropos');
 });
