bb724fa5d964a9ba55c9792a01f062925b38d7c9
Samuel-Gassama <samgassa@hotmail.fr>
2023-02-07 20:31:16 -0500

    Détails contact, pages

--- resources/views/Partials/contact.blade.php
+++ resources/views/Partials/contact.blade.php
@@ -4,8 +4,8 @@
 <div class="container" data-aos="fade-up">
 
   <div class="section-header">
-    <h2>Contact Us</h2>
-    <p>Nihil officia ut sint molestiae tenetur.</p>
+    <h2>Nous contacter</h2>
+    <p>Envoyez-nous un message !</p>
   </div>
 
   <div class="row contact-info">
@@ -13,16 +13,18 @@
     <div class="col-md-4">
       <div class="contact-address">
         <i class="bi bi-geo-alt"></i>
-        <h3>Address</h3>
-        <address>A108 Adam Street, NY 535022, USA</address>
+        <h3>Addresse</h3>
+        <address>
+            515, de la Briquette, Dégelis, QC, Canada 
+        </address>
       </div>
     </div>
 
     <div class="col-md-4">
       <div class="contact-phone">
         <i class="bi bi-phone"></i>
-        <h3>Phone Number</h3>
-        <p><a href="tel:+155895548855">+1 5589 55488 55</a></p>
+        <h3>Numéro de téléphone</h3>
+        <p><a href="tel:+155895548855">+1 (418) 750-2816 </a></p>
       </div>
     </div>
 
@@ -30,7 +32,7 @@
       <div class="contact-email">
         <i class="bi bi-envelope"></i>
         <h3>Email</h3>
-        <p><a href="mailto:info@example.com">info@example.com</a></p>
+        <p><a href="mailto:info@example.com">saloncanindegelis@gmail.com</a></p>
       </div>
     </div>
 
@@ -40,24 +42,24 @@
     <form action="forms/contact.php" method="post" role="form" class="php-email-form">
       <div class="row">
         <div class="form-group col-md-6">
-          <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+          <input type="text" name="name" class="form-control" id="name" placeholder="Votre nom" required>
         </div>
         <div class="form-group col-md-6 mt-3 mt-md-0">
-          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+          <input type="email" class="form-control" name="email" id="email" placeholder="Votre email" required>
         </div>
       </div>
       <div class="form-group mt-3">
-        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+        <input type="text" class="form-control" name="subject" id="subject" placeholder="Sujet" required>
       </div>
       <div class="form-group mt-3">
-        <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+        <textarea class="form-control" name="message" rows="5" placeholder="Votre message" required></textarea>
       </div>
       <div class="my-3">
-        <div class="loading">Loading</div>
+        <div class="loading">Chargement</div>
         <div class="error-message"></div>
-        <div class="sent-message">Your message has been sent. Thank you!</div>
+        <div class="sent-message">Votre message à été envoyé. Merci beaucoup !</div>
       </div>
-      <div class="text-center"><button type="submit">Send Message</button></div>
+      <div class="text-center"><button type="submit">Envoyer</button></div>
     </form>
   </div>
 
