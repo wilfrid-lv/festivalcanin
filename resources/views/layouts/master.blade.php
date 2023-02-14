d46116b6d89e16b9c126af225bba3569788e9d2f
Samuel-Gassama <samgassa@hotmail.fr>
2023-02-14 18:39:49 -0500

    Fix master asset path

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
     <!-- Vendor CSS Files -->
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="{{asset('/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{assets('/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+    <link href="{{asset('/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
     <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
     <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
