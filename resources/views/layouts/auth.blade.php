14c53d4845a42b1478df2b40f1879902e26d9f5f
Samuel-Gassama <samgassa@hotmail.fr>
2023-02-15 08:34:39 -0500

    Fix du merge | assets path & css

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -42,13 +42,13 @@
                     @guest
                         @if (Route::has('login'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Se connecter') }}</a>
                             </li>
                         @endif
 
                         @if (Route::has('register'))
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __("S'inscrire") }}</a>
                             </li>
                         @endif
                     @else
