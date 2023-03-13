3b1063c42342791189585b5557008951dca349e1
Samuel-Gassama <samgassa@hotmail.fr>
2023-03-13 11:10:28 -0400

    Partial header added in the home page

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -10,37 +10,7 @@
 
 <body>
 
-  <!-- ======= Header ======= -->
-
-  <header id="header" class="d-flex align-items-center ">
-    <div class="container-fluid container-xxl d-flex align-items-center">
-
-      <div id="logo" class="me-auto">
-        <!-- Uncomment below if you prefer to use a text logo -->
-         <h1><a href="/">Festival<span>Canin</span></a></h1>
-{{--        <a href="index.html" class="scrollto"><img src="template/assets/img/favicon.ico" alt="" title=""></a>--}}
-      </div>
-
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a class="nav-link scrollto" href="#hero">Accueil</a></li>
-          <li><a class="nav-link scrollto" href="#about">À propos</a></li>
-          <li><a class="nav-link scrollto" href="#speakers">Nos exposants</a></li>
-            <li><a class="nav-link scrollto" href="/activites">Activités</a></li>
-            <li><a class="nav-link scrollto" href="#schedule">Horaire</a></li>
-          <li><a class="nav-link scrollto" href="#gallery">Galerie</a></li>
-          <li><a class="nav-link scrollto" href="#supporters">Partenaires</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
-
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-      <a class="buy-tickets scrollto" href="https://lepointdevente.com/"  target="_blank" >Achetez votre place</a>
-        <a class="login-menu scrollto" href="/login">Connexion</a>
-
-
-    </div>
-  </header><!-- End Header -->
+@include('partials.header')
 
   <!-- ======= Hero Section ======= -->
   <section id="hero">
@@ -293,6 +263,6 @@
 
   @include('partials.js')
 
-</body> h-75
+</body>
 
 </html>
