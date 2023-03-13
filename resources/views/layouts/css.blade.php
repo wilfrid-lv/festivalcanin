9ff28874b438c4b5d6e401c2cbc941ecda845d68
Samuel-Gassama <samgassa@hotmail.fr>
2023-03-13 10:51:55 -0400

    Ajout partials css and js

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -2,23 +2,30 @@
 <html lang="en">
 
 <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
+    <title>Festival Canin de Dégelis</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
     <!-- Favicons -->
     <link href="assets/img/favicon.png" rel="icon">
     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
+
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
 
+
     <!-- Template Main CSS File -->
-    <link rel="stylesheet" type="text/css" href="{{ asset('/vendor/bootstrap-icons/bootstrap-icons.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/swiper/swiper-bundle.min.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('vendor/glightbox/css/glightbox.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/aos/aos.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/swiper/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/vendor/glightbox/css/glightbox.min.css') }}">
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/vendor/bootstrap/css/bootstrap.min.css') }}">
-
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/assets//css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('template/assets//vendor/bootstrap/css/bootstrap.min.css') }}">
 
 </head>
 
