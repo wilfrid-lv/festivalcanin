9727c1db3b73aac2c380263b1d3bf5245691ffe9
Samuel-Gassama <samgassa@hotmail.fr>
2023-03-15 08:22:54 -0400

    Changement de la migration user, modif avec la migration utilisateurs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
+            $table->string('nom')->nullable();
+            $table->text('description')->nullable();
+            $table->string('imagePath')->nullable();
+            $table->string('adresse')->nullable();
+            $table->string('ville')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
